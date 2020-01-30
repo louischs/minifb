bac4f781273c4253eaee71d1ac6eeccdc98fa25f
Louis <louis.chs@eduge.ch>
2020-01-30 14:08:04 +0100

    Navbar

---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>The HTML5 Herald</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="background-color: #3b5998;">
+        <a class="navbar-brand" href="#"><i class="fab fa-bootstrap"></i></a>
+        <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-primary my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+            </form>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#"><i class="fas fa-home"></i> &nbsp Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fas fa-plus"></i> &nbsp Post</a>
+                </li>     
+            </ul>
+            <ul class="navbar-nav my-1">
+            <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fas fa-user-circle"></i></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <script src="js/scripts.js"></script>
+    <script src="https://kit.fontawesome.com/1f3067fe48.js" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
